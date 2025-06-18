4e326b151fad3d7f41c237d45519d2a489c4a75b
1fantom <127671626+1fantom@users.noreply.github.com>
2025-06-18 07:13:44 +0300

    Delete docs directory

--- docs/js/loadHeader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('header.html')
-    .then(res => res.ok ? res.text() : Promise.reject('Fetch error'))
-    .then(html => {
-      document.getElementById('header-placeholder').innerHTML = html;
-    })
-    .catch(console.error);
-});
